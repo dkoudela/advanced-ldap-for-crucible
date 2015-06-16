97f8ff130785771ecdbd0c75649bd50e132da7ae
David Koudela <dkoudela@volny.cz>
2015-06-16 07:46:08 -0700

    Added Unit Tests.

--- src/test/java/ut/com/davidkoudela/crucible/ldap/model/AdvancedLdapGroupBuilderTest.java
+++ src/test/java/ut/com/davidkoudela/crucible/ldap/model/AdvancedLdapGroupBuilderTest.java
@@ -2,7 +2,9 @@ package ut.com.davidkoudela.crucible.ldap.model;
 
 import com.davidkoudela.crucible.config.AdvancedLdapPluginConfiguration;
 import com.davidkoudela.crucible.ldap.connect.AdvancedLdapConnector;
+import com.davidkoudela.crucible.ldap.model.AdvancedLdapGroup;
 import com.davidkoudela.crucible.ldap.model.AdvancedLdapGroupBuilder;
+import com.davidkoudela.crucible.ldap.model.AdvancedLdapPerson;
 import com.davidkoudela.crucible.ldap.model.AdvancedLdapPersonBuilder;
 import com.unboundid.ldap.sdk.Attribute;
 import com.unboundid.ldap.sdk.SearchRequest;
@@ -17,6 +19,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Description: Testing {@link AdvancedLdapGroupBuilder}
@@ -60,36 +63,88 @@ public class AdvancedLdapGroupBuilderTest extends TestCase {
         this.argumentCaptorSearchRequest = ArgumentCaptor.forClass(SearchRequest.class);
         this.argumentCaptorAdvancedLdapPersonBuilder = ArgumentCaptor.forClass(AdvancedLdapPersonBuilder.class);
 
-        this.advancedLdapPluginConfiguration.setUIDAttributeKey("sAMAccountName");
-        this.advancedLdapPluginConfiguration.setDisplayNameAttributeKey("displayName");
-        this.advancedLdapPluginConfiguration.setEmailAttributeKey("email");
+        this.advancedLdapPluginConfiguration.setGIDAttributeKey("sAMAccountName");
+        this.advancedLdapPluginConfiguration.setGroupDisplayNameKey("name");
         this.advancedLdapPluginConfiguration.setUserGroupNamesKey("memberOf");
-        this.advancedLdapPluginConfiguration.setGroupFilterKey("(&(objectCategory=cn=Group*)(sAMAccountName=*))");
+        this.advancedLdapPluginConfiguration.setUserFilterKey("(&(objectCategory=cn=Person*)(sAMAccountName=${USERNAME}))");
 
         Collection<Attribute> attributes = new ArrayList<Attribute>();
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getUIDAttributeKey(), "dkoudela"));
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getDisplayNameAttributeKey(), "David Koudela"));
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getEmailAttributeKey(), "dkoudela@example.com"));
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getUserGroupNamesKey(), "ou=group, ou=example, dc=com", "ou=product, ou=example, dc=com"));
-        this.searchResultEntry = new SearchResultEntry("cn=dkoudela, ou=group, ou=example, dc=com", attributes);
+        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getGIDAttributeKey(), "group"));
+        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getGroupDisplayNameKey(), "Default Group"));
+        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getUserNamesKey(), "cn=dkoudela, ou=group, ou=example, dc=com", "cn=dkoudela, ou=product, ou=example, dc=com"));
+        this.searchResultEntry = new SearchResultEntry("ou=group, ou=example, dc=com", attributes);
 
         attributes = new ArrayList<Attribute>();
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getUIDAttributeKey(), "jdoe"));
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getDisplayNameAttributeKey(), "John Doe"));
-        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getEmailAttributeKey(), "jdoe@example.com"));
-        this.searchResultEntry2 = new SearchResultEntry("cn=jdoe, ou=group, ou=example, dc=com", attributes);
+        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getGIDAttributeKey(), "product"));
+        attributes.add(new Attribute(this.advancedLdapPluginConfiguration.getGroupDisplayNameKey(), "Product Group"));
+        this.searchResultEntry2 = new SearchResultEntry("ou=product, ou=example, dc=com", attributes);
     }
 
     @Test
     public void testHandlePagedSearchResultOneEntryDoNotFollowMembers() {
+        this.advancedLdapGroupBuilderDummy = new AdvancedLdapGroupBuilderDummy(this.advancedLdapPluginConfiguration, false);
+        this.advancedLdapGroupBuilderDummy.setAdvancedLdapConnector(this.advancedLdapConnector);
+        Mockito.doNothing().when(this.advancedLdapConnector).ldapPagedSearch(this.argumentCaptorAdvancedLdapPluginConfiguration.capture(),
+                this.argumentCaptorSearchRequest.capture(), this.argumentCaptorAdvancedLdapPersonBuilder.capture());
+
+
+        this.advancedLdapGroupBuilderDummy.handlePagedSearchResult(this.searchResultEntry);
+
+        List<AdvancedLdapGroup> advancedLdapGroup = this.advancedLdapGroupBuilderDummy.getGroups();
+        assertEquals(1, advancedLdapGroup.size());
+        assertEquals("group", advancedLdapGroup.get(0).getGID());
+        assertEquals("Default Group", advancedLdapGroup.get(0).getDisplayName());
     }
 
     @Test
     public void testHandlePagedSearchResultTwoEntryDoNotFollowMembers() {
+        this.advancedLdapGroupBuilderDummy = new AdvancedLdapGroupBuilderDummy(this.advancedLdapPluginConfiguration, false);
+        this.advancedLdapGroupBuilderDummy.setAdvancedLdapConnector(this.advancedLdapConnector);
+        Mockito.doNothing().when(this.advancedLdapConnector).ldapPagedSearch(this.argumentCaptorAdvancedLdapPluginConfiguration.capture(),
+                this.argumentCaptorSearchRequest.capture(), this.argumentCaptorAdvancedLdapPersonBuilder.capture());
+
+
+        this.advancedLdapGroupBuilderDummy.handlePagedSearchResult(this.searchResultEntry);
+        this.advancedLdapGroupBuilderDummy.handlePagedSearchResult(this.searchResultEntry2);
+
+        List<AdvancedLdapGroup> advancedLdapGroups = this.advancedLdapGroupBuilderDummy.getGroups();
+        assertEquals(2, advancedLdapGroups.size());
+        assertEquals("group", advancedLdapGroups.get(0).getGID());
+        assertEquals("Default Group", advancedLdapGroups.get(0).getDisplayName());
+
+        assertEquals("product", advancedLdapGroups.get(1).getGID());
+        assertEquals("Product Group", advancedLdapGroups.get(1).getDisplayName());
     }
 
     @Test
     public void testHandlePagedSearchResultOneEntryFollowTwoMembersWithOneMemberEntryInEachMember() {
+        this.advancedLdapGroupBuilderDummy = new AdvancedLdapGroupBuilderDummy(this.advancedLdapPluginConfiguration, true);
+        this.advancedLdapGroupBuilderDummy.setAdvancedLdapConnector(this.advancedLdapConnector);
+        this.advancedLdapGroupBuilderDummy.setAdvancedLdapPersonBuilder(this.advancedLdapPersonBuilder);
+        Mockito.doNothing().when(this.advancedLdapConnector).ldapPagedSearch(this.argumentCaptorAdvancedLdapPluginConfiguration.capture(),
+                this.argumentCaptorSearchRequest.capture(), this.argumentCaptorAdvancedLdapPersonBuilder.capture());
+        List<AdvancedLdapPerson> personList = new ArrayList<AdvancedLdapPerson>();
+        AdvancedLdapPerson advancedLdapPerson = new AdvancedLdapPerson();
+        advancedLdapPerson.setUid("dkoudela");
+        advancedLdapPerson.setEmail("dkoudela@example.com");
+        advancedLdapPerson.setDisplayName("David Koudela");
+        personList.add(advancedLdapPerson);
+        Mockito.when(this.advancedLdapPersonBuilder.getPersons()).thenReturn(personList);
+
+        this.advancedLdapGroupBuilderDummy.handlePagedSearchResult(this.searchResultEntry);
+
+        List<AdvancedLdapGroup> advancedLdapGroups = this.advancedLdapGroupBuilderDummy.getGroups();
+        assertEquals(1, advancedLdapGroups.size());
+        assertEquals("group", advancedLdapGroups.get(0).getGID());
+        assertEquals("Default Group", advancedLdapGroups.get(0).getDisplayName());
+
+        assertEquals(2, advancedLdapGroups.get(0).getPersonList().size());
+        assertEquals("dkoudela", advancedLdapGroups.get(0).getPersonList().get(0).getUid());
+        assertEquals("dkoudela@example.com", advancedLdapGroups.get(0).getPersonList().get(0).getEmail());
+        assertEquals("David Koudela", advancedLdapGroups.get(0).getPersonList().get(0).getDisplayName());
+        assertEquals("dkoudela", advancedLdapGroups.get(0).getPersonList().get(1).getUid());
+        assertEquals("dkoudela@example.com", advancedLdapGroups.get(0).getPersonList().get(1).getEmail());
+        assertEquals("David Koudela", advancedLdapGroups.get(0).getPersonList().get(1).getDisplayName());
     }
 
     @Test
