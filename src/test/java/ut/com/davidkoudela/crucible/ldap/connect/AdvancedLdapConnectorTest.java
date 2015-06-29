4b676c3338112af821989fe4f1123a7c88b6795c
David Koudela <dkoudela@volny.cz>
2015-06-29 12:46:05 -0700

    Added Unit Tests.

--- src/test/java/ut/com/davidkoudela/crucible/ldap/connect/AdvancedLdapConnectorTest.java
+++ src/test/java/ut/com/davidkoudela/crucible/ldap/connect/AdvancedLdapConnectorTest.java
@@ -2,16 +2,23 @@ package ut.com.davidkoudela.crucible.ldap.connect;
 
 import com.davidkoudela.crucible.config.AdvancedLdapPluginConfiguration;
 import com.davidkoudela.crucible.ldap.connect.AdvancedLdapConnector;
-import com.unboundid.ldap.sdk.LDAPConnection;
-import com.unboundid.ldap.sdk.LDAPException;
-import com.unboundid.ldap.sdk.SearchResult;
+import com.davidkoudela.crucible.ldap.connect.AdvancedLdapSearchFilterFactory;
+import com.davidkoudela.crucible.ldap.connect.AdvancedLdapSearchResultBuilder;
+import com.davidkoudela.crucible.ldap.model.AdvancedLdapGroupBuilder;
+import com.unboundid.ldap.sdk.*;
 import junit.framework.TestCase;
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description: Testing {@link AdvancedLdapConnector}
@@ -25,6 +32,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class AdvancedLdapConnectorTest extends TestCase {
     private static AdvancedLdapPluginConfiguration advancedLdapPluginConfiguration;
     private static AdvancedLdapConnectorDummy advancedLdapConnector;
+    private static AdvancedLdapSearchResultBuilder advancedLdapSearchResultBuilder;
     private LDAPConnection ldapConnection;
     private SearchResult searchResult;
 
@@ -44,17 +52,30 @@ public class AdvancedLdapConnectorTest extends TestCase {
         this.advancedLdapPluginConfiguration.setLDAPUrl("ldap://localhost:389");
         this.advancedLdapPluginConfiguration.setConnectTimeoutMillis(1);
         this.advancedLdapPluginConfiguration.setResponseTimeoutMillis(2);
+        this.advancedLdapPluginConfiguration.setUserFilterKey("(&(objectCategory=cn=Person*)(sAMAccountName=${USERNAME}))");
         this.ldapConnection = PowerMock.createMock(LDAPConnection.class);
         this.searchResult = PowerMock.createMock(SearchResult.class);
-        this.searchResult = null;
+        this.advancedLdapSearchResultBuilder = Mockito.mock(AdvancedLdapGroupBuilder.class);
     }
     @Test
     public void testLdapPagedSearchNoEntries() throws LDAPException {
         this.advancedLdapConnector = new AdvancedLdapConnectorDummy(this.advancedLdapPluginConfiguration);
         this.advancedLdapConnector.setLdapConnection(this.ldapConnection);
+        SearchRequest searchRequest = new SearchRequest(advancedLdapPluginConfiguration.getLDAPBaseDN(), SearchScope.SUB,
+                AdvancedLdapSearchFilterFactory.getSearchFilterForAllUsers(this.advancedLdapPluginConfiguration.getUserFilterKey()));
+        List<SearchResultEntry> searchResultEntryList = new ArrayList<SearchResultEntry>();
+        Control[] control = new Control[1];
+        control[0] = new Control("1.2.840.113556.1.4.319", true);
+        EasyMock.expect(this.ldapConnection.search(searchRequest)).andReturn(this.searchResult);
+        EasyMock.expect(this.searchResult.getEntryCount()).andReturn(0);
+        EasyMock.expect(this.searchResult.getSearchEntries()).andReturn(searchResultEntryList);
+        EasyMock.expect(this.searchResult.getResponseControls()).andReturn(control);
+        EasyMock.expect(this.searchResult.getResponseControl("1.2.840.113556.1.4.319")).andReturn(control[0]);
+        this.ldapConnection.close();
+        EasyMock.expectLastCall();
 
-        PowerMock.replay(this.ldapConnection);
-//        PowerMock.expect(this.ldapConnection.search()).andReturn(this.searchResult);
-        PowerMock.verify(this.ldapConnection);
+        PowerMock.replayAll();
+        this.advancedLdapConnector.ldapPagedSearch(searchRequest, this.advancedLdapSearchResultBuilder);
+        PowerMock.verifyAll();
     }
 }
