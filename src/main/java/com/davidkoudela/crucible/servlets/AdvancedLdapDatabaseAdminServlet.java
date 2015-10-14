6e6f35f6e2b94cfba50ad30c9d35c1cf6fc41948
David Koudela <dkoudela@volny.cz>
2015-10-14 06:58:45 -0700

    First attempt for Database configuration servlet.

---
+++ src/main/java/com/davidkoudela/crucible/servlets/AdvancedLdapDatabaseAdminServlet.java
@@ -0,0 +1,90 @@
+package com.davidkoudela.crucible.servlets;
+
+import com.atlassian.fisheye.plugin.web.helpers.VelocityHelper;
+import com.davidkoudela.crucible.admin.AdvancedLdapUserManager;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description: Configuration servlet class reacting on HTTP GET and POST requests related to
+ *              database settings.
+ * Copyright (C) 2015 David Koudela
+ *
+ * @author dkoudela
+ * @since 2015-09-10
+ */
+public class AdvancedLdapDatabaseAdminServlet extends HttpServlet {
+    private final VelocityHelper velocityHelper;
+    private AdvancedLdapUserManager advancedLdapUserManager;
+    private String testResponse = "{ " +
+            "    \"errorMsg\": \"No error\", " +
+            "    \"worked\": true, " +
+            "    \"payload\": \"  \\u003cdiv class=\\\"message-success message-status\\\"\\u003e\\n            \\u003cdiv\\u003eConnection succeeded\\u003c/div\\u003e\\n            \\u003ca class=\\\"close\\\"\\u003eclose\\u003c/a\\u003e\\n        \\u003c/div\\u003e\\n        \\u003cdiv class=\\\"db_test\\\"\\u003e\\n            \\u003cbutton id=\\\"db_test\\\" value=\\\"Test Connection\\\" class=\\\"aui-button\\\"\\u003eTest Connection\\u003c/button\\u003e\\n        \\u003c/div\\u003e\\n    \",\n" +
+            "    \"connection\": true, " +
+            "    \"sameUrl\": true, " +
+            "    \"changed\": false, " +
+            "    \"message\": null " +
+            "}";
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public AdvancedLdapDatabaseAdminServlet(VelocityHelper velocityHelper,
+                                                AdvancedLdapUserManager advancedLdapUserManager) {
+        this.velocityHelper = velocityHelper;
+        this.advancedLdapUserManager = advancedLdapUserManager;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String,Object> params = new HashMap<String,Object>();
+        if (advancedLdapUserManager.hasSysAdminPrivileges(req)) {
+            if (req.getPathInfo().contains("/advancedLdapDatabaseAdminServletTest.do")) {
+                setRestDecorator(req, resp);
+                params.put("testResult", this.testResponse);
+                velocityHelper.renderVelocityTemplate("templates/databaseTest.vm", params, resp.getWriter());
+            } else {
+                setAdminMenuDecorator(req, resp);
+                velocityHelper.renderVelocityTemplate("templates/databaseView.vm", params, resp.getWriter());
+            }
+        } else {
+            setAdminMenuDecorator(req, resp);
+            resp.sendRedirect(req.getContextPath() + "/admin/login-default.do");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String,Object> params = new HashMap<String,Object>();
+        if (advancedLdapUserManager.hasSysAdminPrivileges(req)) {
+            if (req.getPathInfo().contains("/advancedLdapDatabaseAdminServletEdit.do")) {
+                setAdminMenuDecorator(req, resp);
+                velocityHelper.renderVelocityTemplate("templates/databaseEdit.vm", params, resp.getWriter());
+            } else if (req.getPathInfo().contains("/advancedLdapDatabaseAdminServletTest.do")) {
+                setRestDecorator(req, resp);
+                params.put("testResult", this.testResponse);
+                velocityHelper.renderVelocityTemplate("templates/databaseTest.vm", params, resp.getWriter());
+            } else {
+                setAdminMenuDecorator(req, resp);
+                velocityHelper.renderVelocityTemplate("templates/databaseView.vm", params, resp.getWriter());
+            }
+        } else {
+            setAdminMenuDecorator(req, resp);
+            resp.sendRedirect(req.getContextPath() + "/admin/login-default.do");
+        }
+    }
+
+    protected void setAdminMenuDecorator(HttpServletRequest req, HttpServletResponse resp) {
+        req.setAttribute("decorator", "atl.admin");
+        resp.setContentType("text/html");
+    }
+
+    protected void setRestDecorator(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("application/json");
+    }
+}
