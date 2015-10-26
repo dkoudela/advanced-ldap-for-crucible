52cfa9a5fa44eae853eda3f4095bce3b7b6810a1
David Koudela <dkoudela@volny.cz>
2015-10-26 15:09:10 -0700

    Added remove button for reseting to default Database setings.

--- src/main/java/com/davidkoudela/crucible/persistence/AdvancedLdapDatabaseConfigurationDAOImpl.java
+++ src/main/java/com/davidkoudela/crucible/persistence/AdvancedLdapDatabaseConfigurationDAOImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
  */
 @Component("advancedLdapDatabaseConfigurationDAO")
 public class AdvancedLdapDatabaseConfigurationDAOImpl implements AdvancedLdapDatabaseConfigurationDAO {
+    private static final String enabled = "AdvancedLdap.dbConfig.enabled";
     private static final String databaseName = "AdvancedLdap.dbConfig.databaseName";
     private static final String username = "AdvancedLdap.dbConfig.username";
     private static final String password = "AdvancedLdap.dbConfig.password";
@@ -26,49 +27,63 @@ public class AdvancedLdapDatabaseConfigurationDAOImpl implements AdvancedLdapDat
 
     @Override
     public void store(AdvancedLdapDatabaseConfiguration advancedLdapDatabaseConfiguration) {
-        PluginSettings settings = this.settingsFactory.createGlobalSettings();
-        settings.put(this.databaseName, advancedLdapDatabaseConfiguration.getDatabaseName());
-        settings.put(this.username, advancedLdapDatabaseConfiguration.getUserName());
-        settings.put(this.password, advancedLdapDatabaseConfiguration.getPassword());
+        try {
+            PluginSettings settings = this.settingsFactory.createGlobalSettings();
+            settings.put(this.enabled, Boolean.TRUE.toString());
+            settings.put(this.databaseName, advancedLdapDatabaseConfiguration.getDatabaseName());
+            settings.put(this.username, advancedLdapDatabaseConfiguration.getUserName());
+            settings.put(this.password, advancedLdapDatabaseConfiguration.getPassword());
+        } catch (Exception e) {
+            System.out.println("Cannot store AdvancedLdapDatabaseConfiguration: " + e);
+        }
     }
 
     @Override
     public AdvancedLdapDatabaseConfiguration get() {
         AdvancedLdapDatabaseConfiguration advancedLdapDatabaseConfiguration = new AdvancedLdapDatabaseConfiguration();
-        PluginSettings settings = this.settingsFactory.createGlobalSettings();
 
-        // Get Database configuration from plugin settings first
-        advancedLdapDatabaseConfiguration.setDatabaseName((String) settings.get(this.databaseName));
-        advancedLdapDatabaseConfiguration.setUserName((String) settings.get(this.username));
-        advancedLdapDatabaseConfiguration.setPassword((String) settings.get(this.password));
+        try {
+            PluginSettings settings = this.settingsFactory.createGlobalSettings();
 
-        if (null != advancedLdapDatabaseConfiguration.getDatabaseName() &&
-                null != advancedLdapDatabaseConfiguration.getUserName() &&
-                null != advancedLdapDatabaseConfiguration.getPassword())  {
-            return advancedLdapDatabaseConfiguration;
+            // Get Database configuration from plugin settings first
+            advancedLdapDatabaseConfiguration.setDatabaseName((String) settings.get(this.databaseName));
+            advancedLdapDatabaseConfiguration.setUserName((String) settings.get(this.username));
+            advancedLdapDatabaseConfiguration.setPassword((String) settings.get(this.password));
+
+            if (null != settings.get(this.enabled) &&
+                    0 == ((String) settings.get(this.enabled)).compareTo(Boolean.TRUE.toString()) &&
+                    null != advancedLdapDatabaseConfiguration.getDatabaseName() &&
+                    null != advancedLdapDatabaseConfiguration.getUserName() &&
+                    null != advancedLdapDatabaseConfiguration.getPassword()) {
+                return advancedLdapDatabaseConfiguration;
+            }
+
+            // If it is not found in the plugin settings, try separate database introduced in the first plugin revisions
+            advancedLdapDatabaseConfiguration.setDatabaseName(AdvancedLdapDatabaseConfigFactory.pluginDbName);
+            advancedLdapDatabaseConfiguration.setUserName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getUsername());
+            advancedLdapDatabaseConfiguration.setPassword(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getPassword());
+            if (false != AdvancedLdapDatabaseConfigFactory.verifyDatabaseConfig(advancedLdapDatabaseConfiguration)) {
+                return advancedLdapDatabaseConfiguration;
+            }
+
+            // If the separate database is not found, use the default crucible database
+            advancedLdapDatabaseConfiguration.setDatabaseName(AdvancedLdapDatabaseConfigFactory.extractDatabaseName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig()));
+            advancedLdapDatabaseConfiguration.setUserName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getUsername());
+            advancedLdapDatabaseConfiguration.setPassword(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getPassword());
+        } catch (Exception e) {
+            System.out.println("Cannot get AdvancedLdapDatabaseConfiguration: " + e);
         }
-
-        // If it is not found in the plugin settings, try separate database introduced in the first plugin revisions
-        advancedLdapDatabaseConfiguration.setDatabaseName(AdvancedLdapDatabaseConfigFactory.pluginDbName);
-        advancedLdapDatabaseConfiguration.setUserName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getUsername());
-        advancedLdapDatabaseConfiguration.setPassword(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getPassword());
-        if (false != AdvancedLdapDatabaseConfigFactory.verifyDatabaseConfig(advancedLdapDatabaseConfiguration)) {
-            return advancedLdapDatabaseConfiguration;
-        }
-
-        // If the separate database is not found, use the default crucible database
-        advancedLdapDatabaseConfiguration.setDatabaseName(AdvancedLdapDatabaseConfigFactory.extractDatabaseName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig()));
-        advancedLdapDatabaseConfiguration.setUserName(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getUsername());
-        advancedLdapDatabaseConfiguration.setPassword(AdvancedLdapDatabaseConfigFactory.getCrucibleDefaultDatabaseConfig().getPassword());
 
         return advancedLdapDatabaseConfiguration;
     }
 
     @Override
     public void remove() {
-        PluginSettings settings = this.settingsFactory.createGlobalSettings();
-        settings.remove(this.databaseName);
-        settings.remove(this.username);
-        settings.remove(this.password);
+        try {
+            PluginSettings settings = this.settingsFactory.createGlobalSettings();
+            settings.put(this.enabled, Boolean.FALSE.toString());
+        } catch (Exception e) {
+            System.out.println("Cannot remove AdvancedLdapDatabaseConfiguration: " + e);
+        }
     }
 }
