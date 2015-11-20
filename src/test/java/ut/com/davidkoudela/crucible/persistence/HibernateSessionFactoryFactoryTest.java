57dbaae999815f36eef4fda78b879256c7ce46a3
David Koudela <dkoudela@volny.cz>
2015-11-20 11:14:27 -0800

    Added Unit Tests.

--- src/test/java/ut/com/davidkoudela/crucible/persistence/HibernateSessionFactoryFactoryTest.java
+++ src/test/java/ut/com/davidkoudela/crucible/persistence/HibernateSessionFactoryFactoryTest.java
@@ -1,6 +1,5 @@
 package ut.com.davidkoudela.crucible.persistence;
 
-import com.cenqua.crucible.hibernate.DBType;
 import com.cenqua.crucible.hibernate.DatabaseConfig;
 import com.davidkoudela.crucible.persistence.AdvancedLdapDatabaseConfigFactory;
 import com.davidkoudela.crucible.persistence.HibernateSessionFactoryFactory;
@@ -9,7 +8,9 @@ import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
 
 /**
  * Description: Testing {@link HibernateSessionFactoryFactory}
@@ -33,4 +34,17 @@ public class HibernateSessionFactoryFactoryTest extends TestCase {
         assertNotNull(sessionFactory);
     }
 
+    @Test(expected = Exception.class)
+    public void testCreateHibernateSessionFactoryException() throws Exception {
+        DatabaseConfig databaseConfig2 = Mockito.mock(DatabaseConfig.class);
+        Mockito.when(databaseConfig2.getJdbcDriverClass()).thenThrow(Exception.class);
+        HibernateSessionFactoryFactory.createHibernateSessionFactory(databaseConfig2);
+    }
+
+    @Test(expected = Exception.class)
+    public void testCreateHibernateSessionFactoryThrowable() throws Exception {
+        DatabaseConfig databaseConfig2 = Mockito.mock(DatabaseConfig.class);
+        Mockito.when(databaseConfig2.getJdbcDriverClass()).thenThrow(Error.class);
+        HibernateSessionFactoryFactory.createHibernateSessionFactory(databaseConfig2);
+    }
 }
